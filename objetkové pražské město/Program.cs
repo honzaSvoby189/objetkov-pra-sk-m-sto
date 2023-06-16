8452fb31ffd977712ad4f81cfdac8fbad5d94cc5
honzaSvoby189 <127093410+honzaSvoby189@users.noreply.github.com>
2023-06-16 12:51:09 +0200

    Update Program.cs

--- objetkové pražské město/Program.cs
+++ objetkové pražské město/Program.cs
@@ -1,5 +1,5 @@
-﻿using objetkové_pražské_město;
-
+using objetkové_pražské_město;
+using Spectre.Console;
 string[] poleLinkaA = { "nemocnice Motol", "Petřiny", "Nádraží Veleslaví", "Bořislavka", "Dejvická", "hradčanská", "Malostranská", "Straroměstská", "Můstek", "Náměstí míru ", "Jiřího z ppoděbrad ", "Flora", "žšliského ", "strašnická", "skalka", "depo hostivař", };
 string[] poleLinkaB = {"Zličín","STODŮLKY","LUKA","LUŽINY","¨HŮRKA","NOVÉ BUTOVICE","JINONICE","RADLICKÁ","SMÍCHOVKSÉ NÁDRAŽÍ","ANDĚL","KARLOVO NÁMĚSTÍ","národní třída",
 "MŮSTEK","NÁMĚSTÍ REPUBLIKY","FLORENC","KŘÍŽKOVA","INVALIDOVNA","PALMOVKA","ČESKOMORAVSKÁ","VYSOČANSKÁ","KOLBENOVA","HLOUBĚTÍN","RAJSKÁ ZAHRADA","ČERNÝ MOST"}; 
@@ -9,12 +9,28 @@ Console.WriteLine("zadej linku kterou chceš využit (A/B/C/D");
 string volbaLinky = Console.ReadLine();
 
 
+await AnsiConsole.Progress()
+    .StartAsync(async ctx =>
+    {
+        // Define tasks
+        var task1 = ctx.AddTask("[green]Reticulating splines[/]");
+        var task2 = ctx.AddTask("[green]Folding space[/]");
 
-if(volbaLinky == "A")
+        while (!ctx.IsFinished)
+        {
+            // Simulate some work
+            await Task.Delay(1);
+
+            // Increment
+            task1.Increment(1);
+            task2.Increment(0.5);
+        }
+    });
+if (volbaLinky == "A")
 {
     LinkaMetra LinkaA = new LinkaMetra() { nazevLinky = "A", Aktivní = true, stanice = poleLinkaA, barva = "zelená" };
     LinkaA.NastavStanice();
-    Console.WriteLine("chete vypsat 1. všechny stanice, 2.první a poslední 3. vypiš čas ");
+    Console.WriteLine("chete vypsat 1. všechny stanice, 2.první a poslední 3. vypiš čas  ");
     string volba = Console.ReadLine();
 
     if(volba == "1")
@@ -24,6 +40,10 @@ if(volbaLinky == "A")
     else if(volba == "2")
     {
         LinkaA.vypisPosledniStanice();
+    }
+    else if (volba == "3")
+    {
+        LinkaA.CasCesty();
     }
 }
 
@@ -42,6 +62,10 @@ if (volbaLinky == "B")
     {
         LinkaB.vypisPosledniStanice();
     }
+    else if (volba == "3")
+    {
+        LinkaB.CasCesty();
+    }
 }
 
 if (volbaLinky == "C")
@@ -58,5 +82,9 @@ if (volbaLinky == "C")
     else if (volba == "2")
     {
         LinkaA.vypisPosledniStanice();
+    }
+    else if (volba == "3")
+    {
+        LinkaA.CasCesty();
     }
 }
