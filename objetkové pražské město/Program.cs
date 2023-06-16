0a9ea9bcce8ae232e6c8b197af441b12a3f1fdeb
Jan Svoboda <jan.svoboda@educhem.cz>
2023-06-16 12:30:39 +0200

    Přidejte soubory projektu.

---
+++ objetkové pražské město/Program.cs
@@ -0,0 +1,62 @@
+﻿using objetkové_pražské_město;
+
+string[] poleLinkaA = { "nemocnice Motol", "Petřiny", "Nádraží Veleslaví", "Bořislavka", "Dejvická", "hradčanská", "Malostranská", "Straroměstská", "Můstek", "Náměstí míru ", "Jiřího z ppoděbrad ", "Flora", "žšliského ", "strašnická", "skalka", "depo hostivař", };
+string[] poleLinkaB = {"Zličín","STODŮLKY","LUKA","LUŽINY","¨HŮRKA","NOVÉ BUTOVICE","JINONICE","RADLICKÁ","SMÍCHOVKSÉ NÁDRAŽÍ","ANDĚL","KARLOVO NÁMĚSTÍ","národní třída",
+"MŮSTEK","NÁMĚSTÍ REPUBLIKY","FLORENC","KŘÍŽKOVA","INVALIDOVNA","PALMOVKA","ČESKOMORAVSKÁ","VYSOČANSKÁ","KOLBENOVA","HLOUBĚTÍN","RAJSKÁ ZAHRADA","ČERNÝ MOST"}; 
+string[] poleLinkaC = {  "Letnany", "Prosek", "Střížkov", "Ládví", "Kobylisy", "Nádraží Holešovice", "Vltavská", "Florenc", "Hlavní Nádrazí", "Muzeum", "I.P Pavlova", "Vyšehrad", "Pražského Povstaní", "Pankrác", "Budějovická", "Kačerov", "Roztyly", "chodov", "Opatov ", "háje ", };
+
+Console.WriteLine("zadej linku kterou chceš využit (A/B/C/D");
+string volbaLinky = Console.ReadLine();
+
+
+
+if(volbaLinky == "A")
+{
+    LinkaMetra LinkaA = new LinkaMetra() { nazevLinky = "A", Aktivní = true, stanice = poleLinkaA, barva = "zelená" };
+    LinkaA.NastavStanice();
+    Console.WriteLine("chete vypsat 1. všechny stanice, 2.první a poslední 3. vypiš čas ");
+    string volba = Console.ReadLine();
+
+    if(volba == "1")
+    {
+        LinkaA.VypisVsechnyStanice();
+    }
+    else if(volba == "2")
+    {
+        LinkaA.vypisPosledniStanice();
+    }
+}
+
+if (volbaLinky == "B")
+{
+    LinkaMetra LinkaB = new LinkaMetra() { nazevLinky = "B", Aktivní = true, stanice = poleLinkaB, barva = "žlutá" };
+    LinkaB.NastavStanice();
+    Console.WriteLine("chete vypsat 1. všechny stanice, 2.první a poslední´ 3, vypiš čas ");
+    string volba = Console.ReadLine();
+
+    if (volba == "1")
+    {
+        LinkaB.VypisVsechnyStanice();
+    }
+    else if (volba == "2")
+    {
+        LinkaB.vypisPosledniStanice();
+    }
+}
+
+if (volbaLinky == "C")
+{
+    LinkaMetra LinkaA = new LinkaMetra() { nazevLinky = "A", Aktivní = true, stanice = poleLinkaA, barva = "modrá" };
+    LinkaA.NastavStanice();
+    Console.WriteLine("chete vypsat 1. všechny stanice, 2.první a poslední  3. zjistit čas ");
+    string volba = Console.ReadLine();
+
+    if (volba == "1")
+    {
+        LinkaA.VypisVsechnyStanice();
+    }
+    else if (volba == "2")
+    {
+        LinkaA.vypisPosledniStanice();
+    }
+}
---
+++ objetkové pražské město/linka metra .cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objetkové_pražské_město
+{
+    internal class LinkaMetra
+    {
+        public string nazevLinky { get; set; }
+        public bool Aktivní { get; set; }
+        public string[] stanice { get; set; }
+        public string barva  { get; set; }
+
+        public void VypisVsechnyStanice()
+        {
+            if(stanice.Length > 0)
+            {
+                foreach(var JednaStanice in stanice)
+                {
+                    Console.WriteLine(  JednaStanice);
+                }
+            }
+        }
+
+
+        public void vypisPosledniStanice()
+        {
+            if(stanice.Length >= 2)
+            {
+                Console.WriteLine(stanice[0]);
+                Console.WriteLine(stanice[stanice.Length -1]);
+            }
+        }
+
+
+        public void NastavStanice()
+        {
+            if(barva == "zelená")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (barva == "červená")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+            }
+            else if (barva == "žlutá")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+            }
+            else if (barva == "modrá")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+            }
+            Console.WriteLine($" Vítej v lince metra{nazevLinky}");
+
+        }
+
+        public void CasCesty()
+        {
+            Console.WriteLine("vyber 1 stanicic");
+            string prvníStanice = Console.ReadLine();
+            int indexPrvni = Array.IndexOf(stanice, prvníStanice);
+
+            Console.WriteLine("-------------------");
+
+
+            Console.WriteLine("vyber 2 stanicic");
+            string DruheStanice = Console.ReadLine();
+            int indexDruhe = Array.IndexOf(stanice, DruheStanice);
+            Console.WriteLine("-------------------");
+            int dobaCesty = (indexPrvni - indexDruhe)*2;
+
+            Console.WriteLine( "Doba cesty" + dobaCesty );
+        }
+    }
+}
